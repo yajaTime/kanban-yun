f12e2c72d84a2ebb065755941ad54b4c09ad4498
leeyunjea <yunjea0312@naver.com>
2022-06-21 22:21:25 +0900

    init

---
+++ src/main/java/com/example/kanbanyun/KanbanYunApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbanyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanYunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanYunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kanbanyun/KanbanYunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbanyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanYunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
